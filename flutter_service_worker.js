5c4d40481b5a4ce7293a9d70708b6b4e33b62e0a
PrabhjotAndroid <prabhjot.relay@gmail.com>
2020-04-26 13:33:37 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "edc3a75cd3e294d8a9dcb66d28d51cb6",
 "/": "edc3a75cd3e294d8a9dcb66d28d51cb6",
-"main.dart.js": "d0e0606945cc24686197b353b7b88a98",
+"main.dart.js": "4bbcf6a0ec121fb27831156028ab3f7b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
